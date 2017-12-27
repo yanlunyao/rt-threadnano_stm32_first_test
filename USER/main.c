31e2dad65b5d0b40e033af88cb7692ca37a07c15
yanly <413899391@qq.com>
2017-12-27 18:25:56 +0800

    first rt-thread stm32 example;

--- USER/main.c
+++ USER/main.c
@@ -1,7 +1,8 @@
 #include "sys.h"
-#include "delay.h"
-#include "usart.h"
+//#include "delay.h"
+//#include "usart.h"
 #include "led.h"
+#include "rtthread.h"
 
 //ALIENTEK 探索者STM32F407开发板 实验1
 //跑马灯实验 -库函数版本
@@ -10,67 +11,70 @@
 //广州市星翼电子科技有限公司  
 //作者：正点原子 @ALIENTEK
 
-int main(void)
-{ 
- 
-	delay_init(168);		  //初始化延时函数
-	LED_Init();		        //初始化LED端口
-	
-  /**下面是通过直接操作库函数的方式实现IO控制**/	
-	
+static struct rt_thread led0_thread;//线程控制块
+static struct rt_thread led1_thread;//线程控制块
+ALIGN(RT_ALIGN_SIZE)
+static rt_uint8_t rt_led0_thread_stack[1024];//线程栈 
+static rt_uint8_t rt_led1_thread_stack[1024];//线程栈 
+
+//线程LED0
+static void led0_thread_entry(void* parameter)
+{
 	while(1)
 	{
-	GPIO_ResetBits(GPIOF,GPIO_Pin_9);  //LED0对应引脚GPIOF.9拉低，亮  等同LED0=0;
-	GPIO_SetBits(GPIOF,GPIO_Pin_10);   //LED1对应引脚GPIOF.10拉高，灭 等同LED1=1;
-	delay_ms(500);  		   //延时300ms
-	GPIO_SetBits(GPIOF,GPIO_Pin_9);	   //LED0对应引脚GPIOF.0拉高，灭  等同LED0=1;
-	GPIO_ResetBits(GPIOF,GPIO_Pin_10); //LED1对应引脚GPIOF.10拉低，亮 等同LED1=0;
-	delay_ms(500);                     //延时300ms
+		LED0=0;  
+		rt_thread_delay(RT_TICK_PER_SECOND/5);                //延时 
+        
+		LED0=1;   
+		rt_thread_delay(RT_TICK_PER_SECOND/5);                //延时1s
+	}
+}
+
+//线程LED1
+static void led1_thread_entry(void* parameter)
+{
+	while(1)
+	{
+		LED1=0;  
+		rt_thread_delay(RT_TICK_PER_SECOND/2);                //延时 
+        
+		LED1=1;    
+		rt_thread_delay(RT_TICK_PER_SECOND/2);                //延时 
 	}
 }
 
 
-/**
-*******************下面注释掉的代码是通过 位带 操作实现IO口控制**************************************
-	
 int main(void)
-{ 
- 
-	delay_init(168);		  //初始化延时函数
-	LED_Init();		        //初始化LED端口
-  while(1)
-	{
-     LED0=0;			  //LED0亮
-	   LED1=1;				//LED1灭
-		 delay_ms(500);
-		 LED0=1;				//LED0灭
-		 LED1=0;				//LED1亮
-		 delay_ms(500);
-	 }
-}
-**************************************************************************************************
- **/	
-	
-/**
-*******************下面注释掉的代码是通过 直接操作寄存器 方式实现IO口控制**************************************
-int main(void)
-{ 
- 
-	delay_init(168);		  //初始化延时函数
-	LED_Init();		        //初始化LED端口
-	while(1)
-	{
-     GPIOF->BSRRH=GPIO_Pin_9;//LED0亮
-	   GPIOF->BSRRL=GPIO_Pin_10;//LED1灭
-		 delay_ms(500);
-     GPIOF->BSRRL=GPIO_Pin_9;//LED0灭
-	   GPIOF->BSRRH=GPIO_Pin_10;//LED1亮
-		 delay_ms(500);
+{
+     
+    LED_Init();                     //初始化LED  
+    
+    // 创建静态线程
+    rt_thread_init(&led0_thread,                 	//线程控制块
+                   "led0",                       	//线程名字，在shell里面可以看到
+                   led0_thread_entry,            	//线程入口函数
+                   RT_NULL,                     	//线程入口函数参数
+                   &rt_led0_thread_stack[0],     	//线程栈起始地址
+                   sizeof(rt_led0_thread_stack),  	//线程栈大小
+                   3,    //线程的优先级
+                   20);                         	//线程时间片
+    rt_thread_startup(&led0_thread);             	//启动线程led0_thread  
+				   
+	rt_thread_init(&led1_thread,                 	//线程控制块
+                   "led1",                      	//线程名字，在shell里面可以看到
+                   led1_thread_entry,            	//线程入口函数
+                   RT_NULL,                     	//线程入口函数参数
+                   &rt_led1_thread_stack[0],		//线程栈起始地址
+                   sizeof(rt_led1_thread_stack), 	//线程栈大小
+                   2,    //线程的优先级
+                   20);         				   
 
-	 }
- }	 
-**************************************************************************************************
-**/	
+	rt_thread_startup(&led1_thread);             //启动线程led1_thread 
+    
+ 
+}
+
+
  
 
 
--- USER/stm32f4xx_it.c
+++ USER/stm32f4xx_it.c
@@ -60,13 +60,13 @@ void NMI_Handler(void)
   * @param  None
   * @retval None
   */
-void HardFault_Handler(void)
-{
-  /* Go to infinite loop when Hard Fault exception occurs */
-  while (1)
-  {
-  }
-}
+//void HardFault_Handler(void)
+//{
+//  /* Go to infinite loop when Hard Fault exception occurs */
+//  while (1)
+//  {
+//  }
+//}
 
 /**
   * @brief  This function handles Memory Manage exception.
@@ -125,24 +125,24 @@ void DebugMon_Handler(void)
 {
 }
 
-/**
-  * @brief  This function handles PendSVC exception.
-  * @param  None
-  * @retval None
-  */
-void PendSV_Handler(void)
-{
-}
+///**
+//  * @brief  This function handles PendSVC exception.
+//  * @param  None
+//  * @retval None
+//  */
+//void PendSV_Handler(void)
+//{
+//}
 
-/**
-  * @brief  This function handles SysTick Handler.
-  * @param  None
-  * @retval None
-  */
-void SysTick_Handler(void)
-{
- 
-}
+///**
+//  * @brief  This function handles SysTick Handler.
+//  * @param  None
+//  * @retval None
+//  */
+//void SysTick_Handler(void)
+//{
+// 
+//}
 
 /******************************************************************************/
 /*                 STM32F4xx Peripherals Interrupt Handlers                   */
